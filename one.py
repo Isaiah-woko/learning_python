0ccf75e4c5851212973cdd4008e7169b886314d6
Isaiah woko <wokoisaiah68@gmail.com>
2024-01-01 12:34:35 +0100

    first python code

---
+++ one.py
@@ -0,0 +1,9 @@
+name = input("input your name: ")
+age = float(input("input your age: "))
+
+print(f"your name is {name}, and you are {age} years")
+
+if age < 18:
+	print("you are not an Adult")
+else:
+	print("you are an adult")
